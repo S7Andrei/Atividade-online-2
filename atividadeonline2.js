0fa6cb28477bb56d0019d9b45b4a532589363145
Andrei Silva <91704521+S7Andrei@users.noreply.github.com>
2022-07-07 00:05:18 -0300

    Add files via upload

---
+++ atividadeonline2.js
@@ -0,0 +1,46 @@
+let data = 291122
+let idade = 15
+let participantes = 7
+let maxdeparticipantes = 150000
+
+console.log ("Olá Nerd!! Seja muito Bem Vindo(a) a (CCXP22)!")
+console.log ("Finalmente 2022 chegou!!! O maior 'Comic Con Experice' que faremos vem aí!")
+console.log ("Você piscou e o Lote chegou. Vale lembrar que alguns ingressos já esgotaram, então corra e garanta o seu agora mesmo!")
+console.log ("Corra!!! Pois estão sobrando poucos ingressos!")
+
+console.log("LOTE1(esgotado)             LOTE2                  LOTE3(esgotado)")
+console.log (">Para a particpação, precisamos que você se cadastre")
+console.log ("---Insira suas Credenciais")
+
+
+
+
+console.log ('Qual a sua idade jovem Padawan?: ${idade}}')
+
+if (idade >= 15) {
+    console.log ("Idade válida. Nós o consagramos, mestre Jedi.")
+} else {
+    console.log ("Idade invalida, Nós não o consagramos mestre Jedi.")
+}
+
+
+
+
+console.log ("Insira a data do evento escolhido. Eu espero que um dia você encontre seu próprio sol Ashen One: ${291122}")
+
+if (data !=291122) {
+    console.log ("Data invalida. Ah shit, here we go again") 
+} else {
+    console.log ("Data válida. Quando tudo parecer perdido, procure o que reflete o que são e aquilo que mais desejam")
+}
+
+
+
+
+console.log ("Quantidade de participantes. Penso, logo existo...")
+
+if (participantes <= maxdeparticipantes) {
+    console.log ("Sucesso! Cadastro efetuado.")
+} else {
+    console.log ("A quantidade de participantes chegou ao limite! Tente outro dia.")
+}
